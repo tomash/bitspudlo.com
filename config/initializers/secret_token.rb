b279c4e0f938abc45b93d6e528deb603c84fba6d
Tomasz Stachewicz <tomekrs@o2.pl>
2011-04-23 21:00:52 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitspudlo50::Application.config.secret_token = '320b8caa4146bc4d74e2dbc9c4a913a3cf0f6735aac912a7bdedf9e86167298e79a5daa32dfe3d04bc4e08cc54e72c222a0bcc43fefeebc0c4432344060d97fd'
