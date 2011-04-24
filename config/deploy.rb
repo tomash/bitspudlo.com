2ccf3f59e96981eca5b6cfc4ccae6c76ed126301
Tomasz Stachewicz <tomekrs@o2.pl>
2011-04-24 15:28:53 +0200

    capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+set :application, "bitspudlo50"
+
+set :user, "ror-bitspudlo"  # CHANGE THIS LINE TO USE YOUR OCS USERNAME
+set :use_sudo, false
+
+role :app, "beta.bitspudlo.com"
+role :web, "beta.bitspudlo.com"
+role :db,  "beta.bitspudlo.com", :primary => true
+
+# If you aren't using Subversion to manage your source code, specify
+# your SCM below:
+set :scm, :git
+set :repository, "tomash@bitspudlo.com:/var/git/bitspudlo_spree"
+set :branch, "master"
+set :deploy_via, :remote_cache
+set :scm_verbose, true
+set :env, "production"
+
+require 'bundler/capistrano'
+
+desc "Link in the shared stuff" 
+task :after_update_code do
+  #run "ln -nfs #{deploy_to}/#{shared_dir}/config/application.yml #{release_path}/config/application.yml"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/public/assets #{release_path}/public/assets"
+end
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "cd #{deploy_to}/current; touch tmp/restart.txt"
+  end
+  task :stop, :roles => :app do
+    run "cd #{deploy_to}/current; touch tmp/restart.txt"
+  end
+  task :restart, :roles => :app do
+    run "cd #{deploy_to}/current; touch tmp/restart.txt"
+  end
+    
+  task :migrate, :roles => :app do
+    run "cd #{current_path}; rake db:migrate RAILS_ENV=#{env}"
+  end
+  
+end
+
+
+after "deploy", "deploy:migrate"
