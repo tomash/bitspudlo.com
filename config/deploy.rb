4b6544021aeed9de22e08ea98465b0834b15692d
Tomasz Stachewicz <tomekrs@o2.pl>
2011-04-28 21:57:22 +0200

    paypal config for server, extension version

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ require 'bundler/capistrano'
 
 desc "Link in the shared stuff" 
 task :after_update_code do
-  #run "ln -nfs #{deploy_to}/#{shared_dir}/config/application.yml #{release_path}/config/application.yml"
+  run "ln -nfs #{deploy_to}/#{shared_dir}/config/application.yml #{release_path}/config/application.yml"
   run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
   run "ln -nfs #{deploy_to}/#{shared_dir}/public/assets #{release_path}/public/assets"
 end
