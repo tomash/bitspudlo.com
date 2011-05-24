969fa4c4005ddc69e14359809fc62bc11a8566ce
Tomasz Stachewicz <tomekrs@o2.pl>
2011-05-24 09:42:09 +0200

    too broad catch-all legacy routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Bitspudlo50::Application.routes.draw do
 
-  match ':locale/products/:id' => 'legacy_products#show'
+  #match ':locale/products/:id' => 'legacy_products#show'
+  match '/en/products/:id' => 'legacy_products#show'
+  match '/pl/products/:id' => 'legacy_products#show'
   
-  match ':locale/categories/:id' => 'legacy_categories#show'
+  #match ':locale/categories/:id' => 'legacy_categories#show'
+  match '/en/categories/:id' => 'legacy_categories#show'
+  match '/pl/categories/:id' => 'legacy_categories#show'
   match '/categories/:id' => 'legacy_categories#show'
   
   match '/store/view_order/:id/:token' => 'legacy_orders#show'
