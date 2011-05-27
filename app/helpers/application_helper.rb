245a2d9bb625c19a78274e1a9704c572afbfa992
Tomasz Stachewicz <tomekrs@o2.pl>
2011-05-27 22:29:19 +0200

    styling, static content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def currencied_number(amount)
     Currency.conversion_to_current(amount)
   end
+  
+  def logo(image_path=Spree::Config[:logo])
+    link_to image_tag(image_path), root_path, :id => 'logo'
+  end
 end
