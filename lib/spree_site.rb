b023b7fecaf77a231808ac9dbabe55268db25f76
Tomasz Stachewicz <tomekrs@o2.pl>
2011-06-05 18:17:51 +0200

    do not allow adding to cart more than we have on_hand

--- lib/spree_site.rb
+++ lib/spree_site.rb
@@ -37,6 +37,17 @@ module SpreeSite
         end
       end
       
+      
+      ActiveSupport::Notifications.subscribe('spree.order.contents_changed') do |*args|
+        event = ActiveSupport::Notifications::Event.new(*args)
+        order = event.payload[:order]
+        order.line_items.each do |line_item|
+          if((order.state == "cart") && (line_item.quantity > line_item.variant.on_hand))
+            line_item.update_attribute(:quantity, line_item.variant.on_hand)
+          end
+        end
+      end
+      
     end
     
     def load_tasks
