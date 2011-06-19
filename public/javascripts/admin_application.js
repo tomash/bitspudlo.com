1812ba44a58091ad5af8607c5f6683b07f2adcb0
Tomasz Stachewicz <tomekrs@o2.pl>
2011-06-19 13:35:44 +0200

    some cosmetics of last commit

--- public/javascripts/admin_application.js
+++ public/javascripts/admin_application.js
@@ -2,7 +2,7 @@
   $(document).ready(function(){
     $('#show_images_link').click(function() {
       $('#line-items a.product_image').replaceWith(function() {
-        return '<img src="'+$(this).attr('href')+'" />';
+        return '<br /><img src="'+$(this).attr('href')+'" />';
       });
       return false;
     });
