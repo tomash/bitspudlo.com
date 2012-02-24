3f846519789c8a53ebfd8ef8d13611b4d38794ec
Tomasz Stachewicz <tomekrs@o2.pl>
2012-02-24 14:01:38 +0100

    return 200 (success) for IPN for inexisting (or ebay) orders

--- app/controllers/payment_notifications_controller_decorator.rb
+++ app/controllers/payment_notifications_controller_decorator.rb
@@ -1,3 +1,12 @@
 PaymentNotificationsController.class_eval do
-        before_filter :set_currency_to_pln
-      end
+  before_filter :set_currency_to_pln
+  before_filter :return_for_inexisting_orders
+
+  private
+  def return_for_inexisting_orders
+    @order = Order.find_by_number(params[:invoice])
+    if(@order.nil? || params[:invoice].nil?)
+      render :text => 'not found', :status => 200, :layout => false
+    end
+  end
+end
