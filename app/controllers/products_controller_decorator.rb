6cc86c21b854b421760ded62c6bb87038e022768
Tomasz Stachewicz <tomekrs@o2.pl>
2012-05-18 01:24:25 +0200

    backported 'only show available products'

---
+++ app/controllers/products_controller_decorator.rb
@@ -0,0 +1,20 @@
+ProductsController.class_eval do
+  # this is from Spree 0.70.6, never released and in branch removed from github
+  # TODO: remove after upgrade
+  def show
+    @product = Product.active.find_by_permalink!(params[:id])
+    return unless @product
+
+    @variants = Variant.active.includes([:option_values, :images]).where(:product_id => @product.id)
+    @product_properties = ProductProperty.includes(:property).where(:product_id => @product.id)
+    @selected_variant = @variants.detect { |v| v.available? }
+
+    referer = request.env['HTTP_REFERER']
+
+    if referer && referer.match(HTTP_REFERER_REGEXP)
+      @taxon = Taxon.find_by_permalink($1)
+    end
+
+    respond_with(@product)
+  end
+end
